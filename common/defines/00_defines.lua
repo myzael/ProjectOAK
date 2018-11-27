5c3cecfa1fa01e21de2fe6adc42c4cff75d44a0d
Hegemony <samuelelandini@gmail.com>
2018-11-28 00:41:58 +0100

    poor italia

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -622,7 +622,7 @@ NMilitary = {
 	LAND_EQUIPMENT_RAMP_COST = 15,					
 	NAVAL_EQUIPMENT_BASE_COST = 15,
 	NAVAL_EQUIPMENT_RAMP_COST = 15,
-	AIR_EQUIPMENT_BASE_COST = 15,
+	AIR_EQUIPMENT_BASE_COST = 25,
 	AIR_EQUIPMENT_RAMP_COST = 15,
 	
 	FASTER_ORG_REGAIN_LEVEL = 0.25,
@@ -653,7 +653,7 @@ NMilitary = {
 	
 	FIELD_MARSHAL_ARMY_BONUS_RATIO = 0.5,           -- ratio to apply regular bonuses FM bonuses to armies
 	
-	FIELD_MARSHAL_XP_RATIO = 0.5,					-- xp gain ratio for army group leaders
+	FIELD_MARSHAL_XP_RATIO = 0.8,					-- xp gain ratio for army group leaders
 	
 	GARRISON_ORDER_ARMY_CAP_FACTOR = 3.0,			-- armies gets increased cap when they are garrisoned
 	
