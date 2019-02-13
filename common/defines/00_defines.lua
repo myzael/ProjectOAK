92817126ffd374714fcf84e7c91bf4104d6afcc1
Hegemony <samuelelandini@gmail.com>
2019-02-13 17:13:28 +0100

    offofo mass

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -398,7 +398,7 @@ NMilitary = {
 	
 	
 	MAX_DIVISION_BRIGADE_WIDTH = 5,			-- Max width of regiments in division designer.
-	MAX_DIVISION_BRIGADE_HEIGHT = 4,		-- Max height of regiments in division designer.
+	MAX_DIVISION_BRIGADE_HEIGHT = 5,		-- Max height of regiments in division designer.
 	MAX_DIVISION_SUPPORT_WIDTH = 2,			-- Max width of support in division designer.
 	MAX_DIVISION_SUPPORT_HEIGHT = 4,		-- Max height of support in division designer.
 	
